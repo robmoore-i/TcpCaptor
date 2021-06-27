0db09d4762f99b17ff17c52ee935e43b8b246464
Rob Moore <rmoore@gradle.com>
2021-06-27 18:52:48 +0800

    Create Gradle plugin TestKeystores which registers a task createKeystore.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.nio.file.Paths
+import kotlin.io.path.exists
+
 plugins {
     id("java")
 }
@@ -17,4 +20,61 @@ dependencies {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+enum class KeyAlgorithm {
+    RSA
+}
+
+interface KeystoreDefinitionExtension {
+    val outputFile: Property<String>
+    val keyAlgorithm: Property<KeyAlgorithm>
+    val keySize: Property<Int>
+    val validityPeriodDays: Property<Int>
+    val privateKeyAlias: Property<String>
+    val commonName: Property<String>
+    val organizationalUnit: Property<String>
+    val organization: Property<String>
+    val location: Property<String>
+    val state: Property<String>
+    val twoLetterCountryCode: Property<String>
+    val password: Property<String>
+}
+
+class TestKeystoresPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        val keystore = target.extensions.create<KeystoreDefinitionExtension>("keystore")
+        target.tasks.register("createKeystore") {
+            val outputFile = keystore.outputFile.getOrElse("keystore.jks")
+            onlyIf { !Paths.get(outputFile).toFile().exists() }
+            doLast {
+                target.exec {
+                    val command = "keytool -genkey " +
+                            "-keystore $outputFile " +
+                            "-keyalg ${keystore.keyAlgorithm.getOrElse(KeyAlgorithm.RSA)} " +
+                            "-keysize ${keystore.keySize.getOrElse(2048)} " +
+                            "-validity ${keystore.validityPeriodDays.getOrElse(365)} " +
+                            "-alias ${keystore.privateKeyAlias.getOrElse("privatekey")} " +
+                            "-dname " +
+                            "CN=${keystore.commonName.getOrElse("localhost")}," +
+                            "OU=${keystore.organizationalUnit.getOrElse("Unknown")}," +
+                            "O=${keystore.organization.getOrElse("Unknown")}," +
+                            "L=${keystore.location.getOrElse("Unknown")}," +
+                            "ST=${keystore.state.getOrElse("Unknown")}," +
+                            "C=${keystore.twoLetterCountryCode.getOrElse("Unknown")} " +
+                            "-storepass ${keystore.password.getOrElse("password")}"
+                    commandLine(command.split(" "))
+                }
+            }
+        }
+    }
+}
+
+apply<TestKeystoresPlugin>()
+
+configure<KeystoreDefinitionExtension> {
+    outputFile.set("testing/keystore.jks")
+    privateKeyAlias.set("selfsigned")
+    commonName.set("localhost")
+    location.set("Singapore")
 }
