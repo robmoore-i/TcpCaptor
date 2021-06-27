d9b78a59e928c310d9341d08104e677839c84522
Rob Moore <rmoore@gradle.com>
2021-06-27 19:44:26 +0800

    Add tasks exportCertificate and createTruststore, which provide fully automate test certificate provisioning. UP-TO-DATE etc. is all fucked though - should take a look at that.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import java.nio.file.Paths
-import kotlin.io.path.exists
 
 plugins {
     id("java")
@@ -20,18 +19,23 @@ dependencies {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+    testLogging {
+        showStandardStreams = true
+    }
 }
 
 enum class KeyAlgorithm {
     RSA
 }
 
+interface PrivateKeyExtension {
+    val alias: Property<String>
+}
+
 interface KeystoreDefinitionExtension {
-    val outputFile: Property<String>
     val keyAlgorithm: Property<KeyAlgorithm>
     val keySize: Property<Int>
     val validityPeriodDays: Property<Int>
-    val privateKeyAlias: Property<String>
     val commonName: Property<String>
     val organizationalUnit: Property<String>
     val organization: Property<String>
@@ -39,22 +43,36 @@ interface KeystoreDefinitionExtension {
     val state: Property<String>
     val twoLetterCountryCode: Property<String>
     val password: Property<String>
+    val keystoreFilename: Property<String>
+}
+
+interface TruststoreDefinitionExtension {
+    val certificateFilename: Property<String>
+    val certificateAlias: Property<String>
+    val password: Property<String>
+    val truststoreFilename: Property<String>
 }
 
 class TestKeystoresPlugin : Plugin<Project> {
     override fun apply(target: Project) {
+        val privateKey = target.extensions.create<PrivateKeyExtension>("privateKey")
+        fun privateKeyAlias() = privateKey.alias.getOrElse("privatekey")
+
         val keystore = target.extensions.create<KeystoreDefinitionExtension>("keystore")
-        target.tasks.register("createKeystore") {
-            val outputFile = keystore.outputFile.getOrElse("keystore.jks")
-            onlyIf { !Paths.get(outputFile).toFile().exists() }
+        fun keystorePassword() = keystore.password.getOrElse("password")
+
+        val createKeystore = target.tasks.register("createKeystore") {
+            val keystoreFilename = keystore.keystoreFilename.getOrElse("keystore.jks")
+            onlyIf { !Paths.get(keystoreFilename).toFile().exists() }
+            outputs.file(keystoreFilename)
             doLast {
                 target.exec {
                     val command = "keytool -genkey " +
-                            "-keystore $outputFile " +
+                            "-keystore $keystoreFilename " +
                             "-keyalg ${keystore.keyAlgorithm.getOrElse(KeyAlgorithm.RSA)} " +
                             "-keysize ${keystore.keySize.getOrElse(2048)} " +
                             "-validity ${keystore.validityPeriodDays.getOrElse(365)} " +
-                            "-alias ${keystore.privateKeyAlias.getOrElse("privatekey")} " +
+                            "-alias ${privateKeyAlias()} " +
                             "-dname " +
                             "CN=${keystore.commonName.getOrElse("localhost")}," +
                             "OU=${keystore.organizationalUnit.getOrElse("Unknown")}," +
@@ -62,19 +80,65 @@ class TestKeystoresPlugin : Plugin<Project> {
                             "L=${keystore.location.getOrElse("Unknown")}," +
                             "ST=${keystore.state.getOrElse("Unknown")}," +
                             "C=${keystore.twoLetterCountryCode.getOrElse("Unknown")} " +
-                            "-storepass ${keystore.password.getOrElse("password")}"
+                            "-storepass ${keystorePassword()}"
                     commandLine(command.split(" "))
                 }
             }
+        }
+
+        val truststore = target.extensions.create<TruststoreDefinitionExtension>("truststore")
+
+        val exportCertificate = target.tasks.register("exportCertificate") {
+            val certificateFilename = truststore.certificateFilename.getOrElse("test.crt")
+            onlyIf { !Paths.get(certificateFilename).toFile().exists() }
+            outputs.file(certificateFilename)
+            val keystoreFilename = createKeystore.get().outputs.files.singleFile.path
+            doLast {
+                target.exec {
+                    val exportCertificateCommand = "keytool -exportcert " +
+                            "-keystore $keystoreFilename " +
+                            "-alias ${privateKeyAlias()} " +
+                            "-file $certificateFilename " +
+                            "-storepass ${keystorePassword()}"
+                    commandLine(exportCertificateCommand.split(" "))
+                }
+            }
+            dependsOn(createKeystore)
+        }
+
+        target.tasks.register("createTruststore") {
+            val outputTruststore = truststore.truststoreFilename.getOrElse("truststore.jks")
+            onlyIf { !Paths.get(outputTruststore).toFile().exists() }
+            val certificateFilename = exportCertificate.get().outputs.files.singleFile.path
+            doLast {
+                target.exec {
+                    val createTruststoreCommand = "keytool -import " +
+                            "-file $certificateFilename " +
+                            "-alias ${truststore.certificateAlias.getOrElse("testCA")} " +
+                            "-keystore $outputTruststore " +
+                            "-storepass ${truststore.password.getOrElse("password")} " +
+                            "-noprompt"
+                    commandLine(createTruststoreCommand.split(" "))
+                }
+            }
+            dependsOn(exportCertificate)
         }
     }
 }
 
 apply<TestKeystoresPlugin>()
 
+configure<PrivateKeyExtension> {
+    alias.set("selfsigned")
+}
+
 configure<KeystoreDefinitionExtension> {
-    outputFile.set("testing/keystore.jks")
-    privateKeyAlias.set("selfsigned")
     commonName.set("localhost")
     location.set("Singapore")
+    keystoreFilename.set("src/test/resources/test_keystore.jks")
+}
+
+configure<TruststoreDefinitionExtension> {
+    certificateFilename.set("src/test/resources/server.crt")
+    truststoreFilename.set("src/test/resources/test_truststore.jks")
 }
